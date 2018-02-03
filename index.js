59eab73e8d9e0e2dbc41bf5be2ae07f97071252a
elaine01 <ecumming.g@gmail.com>
2018-02-03 13:55:47 -0800

    updated gitignore

--- index.js
+++ index.js
@@ -6,6 +6,23 @@ wordList = ['happy', 'sad', 'angry', 'excited'];
 
 // * Randomly selects a word and uses the `Word` constructor to store it
 
+var randomNumber = Math.floor(Math.random() * wordList.length);
+var currentWord = wordList[randomNumber];
+
+
+
 // * Prompts the user for each guess and keeps track of the user's remaining guesses
+inquirer.prompt([
+  {
+    type: "confirm",
+    message: "Are you ready to play",
+    name: "confirm",
+    default: true
+  },
+]).then(function(answers) {
+
+  var newWord = new Word(currentWord);
 
 
+
+});
